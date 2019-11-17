d1708ba2e0f168544f0347b4e113a21e9df22205
Ahmed Agiza <ahmed.agiza22@gmail.com>
2019-11-17 18:45:03 -0500

    Fix TCL Test

--- tests/tcl/gate_cloning.tcl
+++ tests/tcl/gate_cloning.tcl
@@ -1,7 +1,6 @@
 read_lef ../tests/data/libraries/Nangate45/NangateOpenCellLibrary.mod.lef
 read_def ../tests/data/designs/gcd/gcd.def
 read_lib ../tests/data/libraries/Nangate45/NangateOpenCellLibrary_typical.lib
-sta create_clock [sta get_ports clk]  -name core_clock  -period 10
 set_wire_rc 0.0020 0.00020
 # set_log_level trace
 set num_cloned [transform gate_clone 1.5 false]
